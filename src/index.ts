acc2a47de8b854edee2aa8b74c5397e284e2e348
palavalasa vinay kumar <vinaykumarpalavalasa@gmail.com>
2025-02-13 23:27:45 +0530

    third commit

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Middleware
 app.use(express.json());
 
-// Routes
-
-// Default Route
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Express + TypeScript Server!");
 });
 
-// Start Server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
